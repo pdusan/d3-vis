6d7320bce7090b218f1d76fbfb9b9688176f217b
Dusan Popovic <a01651258@unet.univie.ac.at>
2022-10-08 21:44:07 +0200

    created bar chart

--- A1/scripts/vis.js
+++ A1/scripts/vis.js
@@ -3,15 +3,21 @@ let svg = d3.select("#bar-container");
 let margin = {
   top: 30,
   bottom: 30,
-  left: 30,
+  left: 0,
   right: 30,
 };
 
-let width = 1000 - margin.left - margin.right;
-let height = 800 - margin.top - margin.bottom;
+let width =
+  document.getElementById("bar-container").clientWidth -
+  margin.left -
+  margin.right;
+let height =
+  document.getElementById("bar-container").clientHeight -
+  margin.top -
+  margin.bottom;
 
-let xScale = d3.scaleOrdinal().range([0, width]);
-let yScale = d3.scaleLinear().range([0, height]);
+let xScale = d3.scaleBand().range([width + 50, 0]);
+let yScale = d3.scaleLinear().range([height, 0]);
 
 let xAxis = d3.axisBottom().scale(xScale);
 let yAxis = d3.axisLeft().scale(yScale);
@@ -22,17 +28,29 @@ d3.csv("../data/austria_most_visited_places_2018.csv").then((data) => {
   });
 
   const visits = data.map((d) => d.Visitors);
+  const places = data.map((d) => d.Place);
 
   var min = d3.min(visits);
   var max = d3.max(visits);
+
+  yScale.domain([0, max]);
+  xScale.domain(places);
+  xScale.bandwidth(50);
+
+  svg.append("g").call(yAxis).attr("transform", "translate(100, 0)");
+
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("transform", "translate(100, " + height + ")");
 
   svg
     .selectAll("attractions")
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", 50)
-    .attr("y", 50)
-    .attr("width", 10)
-    .attr("height", 40);
+    .attr("x", (d) => xScale(d.Place) + 160)
+    .attr("y", (d) => height - yScale(d.Visitors))
+    .attr("width", 50)
+    .attr("height", (d) => yScale(d.Visitors));
 });
