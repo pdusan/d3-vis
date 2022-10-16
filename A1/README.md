956a00287c4a85ddd93fec4da73a23b4df5a03cc
Dusan Popovic <a01651258@unet.univie.ac.at>
2022-10-16 13:37:45 +0200

    finished assignment 1

---
+++ A1/README.md
@@ -0,0 +1,18 @@
+# VIS Assignment 1 - Dusan Popovic 01651258
+
+All the functionality has been split into its corresponding files (html, css and js), with the main part of the assignment
+located in vis.js, inside the scripts folder. The remaining data and style folders contain the dataset and the stylesheet respectively.
+
+Using d3, the provided data set is loaded in and the resulting bar chart is displayed.
+
+Scaling on the Y axis has been done with a linear scale, with the domain extended a little, so that the highest visitor count doesn't just end at the top of the axis
+which helps us in seeing the actual highest number of visitors.
+
+The X Axis has been scaled useing a band scale, determined by the width of the svg element.
+
+The resulting bars are then drawn, their height determined by scaling the number of visitors with the yScale.
+
+The required lables have been attached to the svg element and positioned around the chart.
+
+Additional Note:
+I have already done this assignment last year (had no issues with it back then) and I wanted to make this one witout referencing the old one at all (mostly just for coding practice). I didn't take any code from my old assignment, but since there's really a limited number of ways one can make a bar chart like this, some parts might be very similar, I hope this won"t be an issue.
--- A1/scripts/vis.js
+++ A1/scripts/vis.js
@@ -1,26 +1,41 @@
 let svg = d3.select("#bar-container");
 
-let margin = {
+let dims = {
   top: 30,
   bottom: 30,
-  left: 0,
-  right: 30,
+  left: 45,
+  right: 100,
+  band: 50,
 };
 
 let width =
-  document.getElementById("bar-container").clientWidth -
-  margin.left -
-  margin.right;
+  document.getElementById("bar-container").clientWidth - dims.left - dims.right;
 let height =
   document.getElementById("bar-container").clientHeight -
-  margin.top -
-  margin.bottom;
+  dims.top -
+  dims.bottom;
 
-let xScale = d3.scaleBand().range([width + 50, 0]);
+let xScale = d3.scaleBand().range([width - dims.left - dims.right, 0]);
 let yScale = d3.scaleLinear().range([height, 0]);
 
 let xAxis = d3.axisBottom().scale(xScale);
 let yAxis = d3.axisLeft().scale(yScale);
+
+let elements = svg
+  .append("g")
+  .attr("transform", "translate(" + dims.right + ", 0)");
+
+svg
+  .append("text")
+  .text("Tourist attractions")
+  .attr("x", width / 2)
+  .attr("y", height + dims.bottom);
+
+svg
+  .append("text")
+  .text("Number of visitors")
+  .attr("x", 0)
+  .attr("y", height / 2);
 
 d3.csv("../data/austria_most_visited_places_2018.csv").then((data) => {
   data.forEach((d) => {
@@ -33,24 +48,28 @@ d3.csv("../data/austria_most_visited_places_2018.csv").then((data) => {
   var min = d3.min(visits);
   var max = d3.max(visits);
 
-  yScale.domain([0, max]);
+  yScale.domain([0, max + min]);
   xScale.domain(places);
-  xScale.bandwidth(50);
 
-  svg.append("g").call(yAxis).attr("transform", "translate(100, 0)");
+  elements
+    .append("g")
+    .call(yAxis)
+    .attr("class", "axis")
+    .attr("transform", "translate(" + dims.right + ", 0)");
 
-  svg
+  elements
     .append("g")
     .call(xAxis)
-    .attr("transform", "translate(100, " + height + ")");
+    .attr("class", "axis")
+    .attr("transform", "translate(" + dims.right + ", " + height + ")");
 
-  svg
+  elements
     .selectAll("attractions")
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", (d) => xScale(d.Place) + 160)
-    .attr("y", (d) => height - yScale(d.Visitors))
-    .attr("width", 50)
-    .attr("height", (d) => yScale(d.Visitors));
+    .attr("x", (d) => xScale(d.Place) + dims.right + dims.left)
+    .attr("y", (d) => yScale(d.Visitors))
+    .attr("width", dims.band)
+    .attr("height", (d) => height - yScale(d.Visitors));
 });
