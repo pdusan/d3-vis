059d9f8b78662aec0331dc0a411a3559ba0fe4f0
Dusan Popovic <a01651258@unet.univie.ac.at>
2022-11-28 19:49:28 +0100

    minor component update

--- A3/src/components/App.js
+++ A3/src/components/App.js
@@ -3,8 +3,19 @@ import "../static/App.css";
 import Slider from "./Slider";
 import ScatterPlot from "./ScatterPlot";
 import Choropleth from "./Choropleth";
+import * as d3 from "d3";
+import mapStatesUsa from "../resources/us-states-geo.json";
 
 function App() {
+  var burglary,
+    income = null;
+  d3.csv("../resources/usa_burglary_rates_1984-2014.csv").then(
+    (data) => (burglary = data)
+  );
+  d3.csv("../resources/usa_disposable_personal_income_1984_2014.csv").then(
+    (data) => (income = data)
+  );
+
   return (
     <div>
       <Choropleth />
