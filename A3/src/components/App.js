5760b7952dd5b12900e5f31e81e248a15534df04
Dusan Popovic <a01651258@unet.univie.ac.at>
2022-11-29 21:38:27 +0100

    finished data loading and transformations

--- A3/src/components/App.js
+++ A3/src/components/App.js
@@ -1,20 +1,68 @@
 import logo from "../resources/logo.svg";
 import "../static/App.css";
+import { useState, useEffect } from "react";
 import Slider from "./Slider";
 import ScatterPlot from "./ScatterPlot";
 import Choropleth from "./Choropleth";
 import * as d3 from "d3";
 import mapStatesUsa from "../resources/us-states-geo.json";
+import burglary from "../resources/usa_burglary_rates_1984-2014.csv";
+import income from "../resources/usa_disposable_personal_income_1984_2014.csv";
+
+/**
+ * Rearrange the input dataset to be more suitable for plotting
+ * @param {Object} data Dataset to convert
+ * @param {String} name The name of the measured value
+ * @returns {Object} Converted data
+ */
+function niceData(data, name) {
+  var col = data.columns.slice(1);
+  data.forEach((d) => {
+    col.forEach((c) => {
+      d[c] = +d[c];
+    });
+  });
+
+  var grouped = d3.group(data, (d) => d["State"]);
+  var betterData = [];
+  Array.from(grouped.keys()).forEach((d) => {
+    let item = {};
+    item.state = d;
+    item.values = [];
+    col.forEach((c) => {
+      let entry = {};
+      entry.year = c;
+      entry[name] = grouped.get(d)[0][c];
+      item.values.push(entry);
+    });
+    betterData.push(item);
+  });
+  return betterData;
+}
 
 function App() {
-  var burglary,
-    income = null;
-  d3.csv("../resources/usa_burglary_rates_1984-2014.csv").then(
-    (data) => (burglary = data)
-  );
-  d3.csv("../resources/usa_disposable_personal_income_1984_2014.csv").then(
-    (data) => (income = data)
-  );
+  const [data, setData] = useState();
+
+  // Both data sets are loaded and transformed according to the above defined function
+  // and then "manually" deep merged, so that each state has a series of years and their
+  // corresponding income and burlary values
+  useEffect(() => {
+    let set = d3
+      .csv(burglary)
+      .then((d) => (set = niceData(d, "burglary")))
+      .then(
+        d3.csv(income).then((s) => {
+          let temp = niceData(s, "income");
+          temp.forEach((i) => {
+            let state = set.filter((o) => o.State === i.State);
+            for (let k = 0; k < i.values.length; k++) {
+              state[0].values[k].income = i.values[k].income;
+            }
+          });
+          setData(set);
+        })
+      );
+  }, []);
 
   return (
     <div>
